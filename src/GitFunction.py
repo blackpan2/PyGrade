506b76ed169e29b47e54d09c05078bc23f6e7c57
George Herde <blackpan2@gmail.com>
2016-03-08 12:25:13 -0500

    Initial commit, moved project to GitHub

---
+++ src/GitFunction.py
@@ -0,0 +1,13 @@
+import subprocess
+
+__author__ = 'George Herde'
+
+def pull():
+    process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+
+def reset():
+    pass
+
+def log():
+    pass
