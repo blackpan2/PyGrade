11ed824de615393319b885c0e3cb13490b422ef0
George Herde <blackpan2@gmail.com>
2016-03-15 15:01:40 -0400

    Fixed bug where sometimes the list of students was not in alphabetical order

--- src/GradeHelp.py
+++ src/GradeHelp.py
@@ -37,6 +37,7 @@ def get_student_directories(start=None):
                     grade_list.append(cur_dir)
             else:
                 excluded.append(cur_dir)
+    grade_list.sort()
     return grade_list, excluded
 
 
